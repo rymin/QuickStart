fc97f42e662c44b8ac7d579950ac29d47ab29f8b
erymin <erymin@diasoft.ru>
2020-12-21 12:18:24 +0300

    refactoring

--- src/main/java/quickstart/microservises/controller/CarController.java
+++ src/main/java/quickstart/microservises/controller/CarController.java
@@ -1,11 +1,19 @@
 package quickstart.microservises.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.h2.util.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import quickstart.microservises.domain.Car;
+import quickstart.microservises.exceptions.NotFoundException;
 import quickstart.microservises.service.CarService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("cars")
@@ -15,30 +23,35 @@ public class CarController {
     private CarService carService;
 
     @GetMapping
-    public ResponseEntity<?> carList(){
-        return carService.getAll();
-    }
+    @ResponseStatus(HttpStatus.OK)
+    public List<Car> carList(){ return carService.getAll(); }
 
     @GetMapping("{id}")
-    public ResponseEntity<?> getOne(@PathVariable Long id){
-        return carService.get(id);
+    @ResponseStatus(HttpStatus.OK)
+    public Car getOne(@PathVariable Long id){
+        return carService.findById(id);
     }
 
-    @PostMapping("/car")
-    public ResponseEntity<?> create(@RequestBody Car car){
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Car create(@RequestBody Car car){
         return carService.create(car);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Car car){
+    public Car update(@PathVariable Long id, @RequestBody Car car){
          return carService.update(id, car);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> delete(@PathVariable long id){
-        return carService.remove(id);
+    public void delete(@PathVariable long id){
+        carService.remove(id);
+    }
+
+    @ExceptionHandler({NotFoundException.class})
+    public void handleResourceNotFoundException(HttpServletResponse response) {
+        response.setStatus(HttpStatus.PRECONDITION_FAILED.value());
     }
 
 }
-
 
--- src/main/java/quickstart/microservises/service/CarService.java
+++ src/main/java/quickstart/microservises/service/CarService.java
@@ -18,48 +18,34 @@ public class CarService {
     @Autowired
     private CarRepository carRepository;
 
-    public ResponseEntity<?> getAll(){
-        return ResponseEntity.status(HttpStatus.OK).body(carRepository.findAll());
+    public Car findById(Long id) {
+        Optional<Car> car = carRepository.findById(id);
+        if (car.isPresent())
+            return car.get();
+        else
+            throw new NotFoundException();
     }
 
-
-    public ResponseEntity<?> get(long id){
-        Optional<Car> entity = carRepository.findById(id);
-
-        if(entity.isPresent()){
-            return ResponseEntity.status(HttpStatus.FOUND).body(entity);
-        }else{
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new NotFoundException().getMessage());
-        }
+    public List<Car> getAll() {
+        return carRepository.findAll();
     }
 
-    public ResponseEntity<?> create(Car car){
+    public Car create(Car car) {
         car.setId(null);
-        Car createdCar = carRepository.save(car);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdCar);
+        return carRepository.save(car);
+
     }
 
-    public ResponseEntity<?> update(Long id, Car car){
-        Optional<Car> entity = carRepository.findById(id);
-
-        if(entity.isPresent()){
-            entity.get().setModel(car.getModel());
-            entity.get().setBrand(car.getBrand());
-            Car updatedCar = carRepository.save(entity.get());
-            return ResponseEntity.status(HttpStatus.OK).body(updatedCar);
-        } else{
-            return ResponseEntity.status(HttpStatus.OK).body(new NotFoundException().getMessage());
-        }
+    public Car update(Long id, Car car) {
+        Car foundedCar = findById(id);
+        foundedCar.setBrand(car.getBrand());
+        foundedCar.setModel(car.getModel());
+        return carRepository.save(foundedCar);
     }
 
-    public ResponseEntity<?> remove(long id){
-        Optional<Car> entity = carRepository.findById(id);
-        if(entity.isPresent()){
-            carRepository.delete(entity.get());
-            return ResponseEntity.status(HttpStatus.OK).body(id);
-        }else{
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new NotFoundException().getMessage());
-        }
+    public void remove(long id) {
+        Car foundedCar = findById(id);
+        carRepository.delete(foundedCar);
     }
 
 }
